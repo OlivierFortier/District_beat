d070b91f51574b4eb09cfce8e2141f3247c23f2f
Olivier Fortier <olivier_fortier@outlook.com>
2020-10-16 12:02:37 -0400

    changement de contrôle source vers github
    unity collab est trop limitant , et moins sécuritaire

---
+++ Assets/collisionController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionController : MonoBehaviour
+{
+    public healthBarController healthBar;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ennemi")
+        {
+            if (healthBar)
+            {
+                healthBar.OnTakeDamage(10);
+            }
+        }
+    }
+}
+
---
+++ Assets/healthBarController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthBarController : MonoBehaviour
+{
+    public GameObject refHealthBar;
+
+    private GameObject instanceHealthBar;
+    public float health;
+    public float startHealth;
+
+    private void Start()
+    {
+        //on instancie le prefab
+        instanceHealthBar = Instantiate(refHealthBar);
+
+        //on parente au canvas l'élément UI
+        instanceHealthBar.transform.SetParent(GameObject.Find("healthBar_Canvas").transform);
+
+        //on le positionne à la meme place que le fond
+        instanceHealthBar.GetComponent<RectTransform>().anchoredPosition =
+            GameObject.Find("healthBar_BG").GetComponent<RectTransform>().anchoredPosition
+        ;
+
+    }
+
+    public void OnTakeDamage(int damage)
+    {
+
+
+        health = health - damage;
+        instanceHealthBar.GetComponent<Image>().fillAmount = health / startHealth;
+    }
+
+}
