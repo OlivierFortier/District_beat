cf698cadcb1fc1b2346b9700a7ac26bbe33e53ac
Olivier Fortier <olivier_fortier@outlook.com>
2020-10-20 13:51:55 -0400

    ajout commentaire script navmesh et ennemi

--- Assets/assets_equipe/scripts/ConstructeurNavmesh.cs
+++ Assets/assets_equipe/scripts/ConstructeurNavmesh.cs
@@ -6,39 +6,48 @@ using UnityEngine.AI;
 public class ConstructeurNavmesh : MonoBehaviour
 {
 
+    //référence aux gameobject qui composent la surface du sol marchable par les joueurs et AI
     public GameObject[] surfacesSol;
 
-    public bool testAllo;
+    //booléen pour déterminer si on veut ou non construire 1 seul navmesh ou 1 navmesh par section
+    public bool construireUnSeulNavMesh;
 
+    //référence au composant NavMesh de l'objet
     public NavMeshSurface solNav;
 
-    // Start is called before the first frame update
     void Start()
     {
-        if (!testAllo)
+        //si on ne veut pas construire un seul navmesh
+        if (!construireUnSeulNavMesh)
         {
+            //trouver toutes les surfaces qui contiennent le tag sol et les mettre dans la variable
             surfacesSol = GameObject.FindGameObjectsWithTag("sol");
 
+            //pour chaque surface trouvée
             foreach (GameObject surface in surfacesSol)
             {
+                //si la surface possède un composant navmesh
                 if (surface.GetComponent<NavMeshSurface>())
                 {
+                    //aller chercher la référence au composant navmesh
                     NavMeshSurface navmeshSurface = surface.GetComponent<NavMeshSurface>();
+                    //nettoyer les données précédentes
+                    navmeshSurface.RemoveData();
+                    //construire le navmesh de la section
                     navmeshSurface.BuildNavMesh();
                 }
             }
         }
         else
         {
-            if (solNav) { 
+            //si le composant navmesh de l'objet global est activé
+            if (solNav)
+            {
+                //nettoyer les données précédentes
                 solNav.RemoveData();
-                solNav.BuildNavMesh(); }
+                //reconstruire le navmesh
+                solNav.BuildNavMesh();
+            }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
