a55225f012c0b04c54306ce52c19927e1079d020
Bernard Dwyer-Beaudet <bernarddwyerb@gmail.com>
2020-10-24 17:04:26 -0400

    ajouter attaque à l'animator. script attaque_melee.

---
+++ Assets/assets_equipe/scripts/combatMelee.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class combatMelee : MonoBehaviour
+{
+    public float tempsEntreAttaque;
+    public float debutAttaque;
+    public Animator animator;
+    public BoxCollider attaqueCollider;
+
+    private void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tempsEntreAttaque <= 0)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                attaque();
+                tempsEntreAttaque = debutAttaque;
+            }
+        }
+        else
+        {
+            tempsEntreAttaque -= Time.deltaTime;
+            animator.SetBool("attaque", false);
+            attaqueCollider.enabled = false;
+        }
+
+    }
+
+    void attaque()
+    {
+        animator.SetBool("attaque", true);
+        attaqueCollider.enabled = true;
+        Debug.Log("pow");
+    }
+}
