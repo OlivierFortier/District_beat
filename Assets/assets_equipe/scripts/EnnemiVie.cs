9049d839aa15f2e188e61a859238b842add5236d
Bernard Dwyer-Beaudet <bernarddwyerb@gmail.com>
2020-10-26 08:38:51 -0400

    script arme, script ennemiVIe (juste test)

---
+++ Assets/assets_equipe/scripts/EnnemiVie.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemiVie : MonoBehaviour
+{
+    public int Enemyhealth; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemyhealth = 100;
+    }
+
+}
---
+++ Assets/assets_equipe/scripts/toucherEnnemi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toucherEnnemi : MonoBehaviour
+{
+    public BoxCollider colliderArme;
+
+    private void OnCollisionEnter(Collision toucherEnnemi)
+    {
+       if (toucherEnnemi.gameObject.tag == "ennemi")
+        {
+            toucherEnnemi.gameObject.Enemyhealth--; 
+        }   
+    }
+}
+
