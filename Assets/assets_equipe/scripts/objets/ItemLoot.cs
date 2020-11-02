7ecdfb53724d8394e73b0b8dc04b4e743aff7922
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-02 16:22:04 -0500

    début script loot

---
+++ Assets/assets_equipe/scripts/objets/ItemLoot.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemLoot : MonoBehaviour
+{
+
+    public enum TypeItem { PackVie, Speed, Morphine, Neon }
+    public TypeItem typeItem;
+
+    private void OnCollisionEnter(Collision entitee)
+    {
+        if (entitee.gameObject.CompareTag("joueur"))
+        {
+            switch (typeItem)
+            {
+                case TypeItem.PackVie:
+                    PrendrePackVie();
+                    break;
+
+                case TypeItem.Speed:
+                    PrendreSpeed();
+                    break;
+
+                case TypeItem.Morphine:
+                    PrendreMorphine();
+                    break;
+
+                case TypeItem.Neon:
+                    PrendreNeon();
+                    break;
+
+                default:
+                    PrendrePackVie();
+                    break;
+            }
+
+        }
+    }
+
+    private void PrendrePackVie()
+    {
+
+    }
+
+    private void PrendreSpeed()
+    {
+
+    }
+
+    private void PrendreMorphine()
+    {
+
+    }
+
+    private void PrendreNeon()
+    {
+
+    }
+
+
+
+
+}
