1f3835feb26928f6898c4b50a078d5723dfd3aa1
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-06 14:28:05 -0500

    barre de vie des ennemis

---
+++ Assets/UIVieEnnemi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIVieEnnemi : MonoBehaviour
+{
+    public GameObject barreVie;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        barreVie.GetComponent<Image>().fillAmount = gameObject.GetComponent<healthBarController>().vie / gameObject.GetComponent<healthBarController>().vieDebut;
+    
+    }
+}
---
+++ Assets/VieFlottante.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VieFlottante : MonoBehaviour
+{
+
+
+    public Transform lookAt;
+
+    public Vector3 offset;
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = cam.WorldToScreenPoint(lookAt.position + offset);
+
+        if(transform.position != pos)
+            transform.position = pos;
+    }
+}
