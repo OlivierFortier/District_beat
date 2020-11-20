7be7553ee8048167652036ee5db1c55595f04552
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-20 12:01:20 -0500

    début gestion du spawn + fix erreur vie

---
+++ Assets/assets_equipe/scripts/Gestion-apparitions/Controlleur_apparition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlleur_apparition : MonoBehaviour
+{
+    private List<GameObject> pointsApparition;
+    
+    void Start()
+    {
+        foreach (var point in transform)
+        {
+            print(point);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
