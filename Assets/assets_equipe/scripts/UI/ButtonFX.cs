1ee80a92993a4717cab2d608cf5fd8dd1fcb7354
Alejandra Miranda <ale.miranda890@gmail.com>
2020-11-21 22:52:53 -0500

    ajout des effets audio aux buttons

---
+++ Assets/assets_equipe/scripts/UI/ButtonFX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonfx : MonoBehaviour
+{
+   public AudioSource myFx;
+   public AUdioClip hoverFx;
+   public AUdioClip clickFx;
+
+   public void HoverSound()
+   {
+       myFx.PlayOneShot(hoverFx);
+   }
+   public void ClickSound()
+   {
+       myFx.PlayOneShot(clickFx);
+   }
+}
