070570990d6340a4ee74dd9ad197c770b7aa4c7e
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-30 22:26:48 -0500

    lazy loader les ennemis

---
+++ Assets/assets_equipe/scripts/Gestion-apparitions/ChargerEnnemis.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargerEnnemis : MonoBehaviour
+{
+
+    public ControlleurApparition controlleur;
+
+    private void OnTriggerEnter(Collider autreObjet) {
+        if(autreObjet.transform.CompareTag("joueur")) {
+
+         
+
+            controlleur.enabled = true;
+            
+        }
+    }
+
+}
