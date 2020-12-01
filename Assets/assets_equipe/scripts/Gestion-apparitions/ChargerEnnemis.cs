dbda75e1c66c7af8345b3c42a33fff76bfcc633c
Olivier Fortier <olivier_fortier@outlook.com>
2020-11-30 22:35:04 -0500

    optimisation script

--- Assets/assets_equipe/scripts/Gestion-apparitions/ChargerEnnemis.cs
+++ Assets/assets_equipe/scripts/Gestion-apparitions/ChargerEnnemis.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class ChargerEnnemis : MonoBehaviour
 {
 
+    // référence au script de création d'ennemis
     public ControlleurApparition controlleur;
 
+    // activer le script de création d'ennemis quand on entre dans la salle
     private void OnTriggerEnter(Collider autreObjet) {
         if(autreObjet.transform.CompareTag("joueur")) {
 
-         
-
             controlleur.enabled = true;
-            
+            Destroy(gameObject, 1f);
         }
     }
 
