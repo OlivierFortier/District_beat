b914b117bb1975a3e8edc6da336471c939487080
alejandra miranda <ale.miranda890@gmail.com>
2020-12-07 10:39:52 -0500

    debut particle systeme gore

--- Assets/assets_equipe/scripts/Combat/toucherEnnemi.cs
+++ Assets/assets_equipe/scripts/Combat/toucherEnnemi.cs
@@ -19,7 +19,10 @@ public class toucherEnnemi : MonoBehaviour
     private bool estunProjectile = false;
     
     //systeme de particule pour le sang
-    public ParticleSystem particuleSang; 
+    //public ParticleSystem particuleSang; 
+    
+    //sons pour les armes
+    public AudioClip slashSon; 
 
     private void Start()
     {
@@ -32,7 +35,7 @@ public class toucherEnnemi : MonoBehaviour
         // si c'Est un projectile, activer le collider après un certain délai
         if (estunProjectile) Invoke("ActiverCollider", 0.15f);
 
-        particuleSang = GetComponent<ParticleSystem>();
+        //particuleSang = GetComponent<ParticleSystem>();
     }
 
     private void OnCollisionEnter(Collision toucherEnnemi)
@@ -45,9 +48,9 @@ public class toucherEnnemi : MonoBehaviour
             {
                 // faire perdre de la vie
                 toucherEnnemi.gameObject.GetComponent<healthBarController>().PrendreDommages(dommagesTotal);
-                particuleSang.Play();
+                //particuleSang.Play();
                 //faire jouer le son d'un slash d'épée lorsque le joueur frappe un ennemi
-                GetComponent<AudioSource>().Play();
+                GetComponent<AudioSource>().PlayOneShot(slashSon, 0.5f);
 
                
 
@@ -61,9 +64,9 @@ public class toucherEnnemi : MonoBehaviour
             {
                 // faire perdre de la vie
                 toucherEnnemi.gameObject.GetComponent<healthBarController>().PrendreDommages(dommagesTotal);
-                particuleSang.Play();
+                //particuleSang.Play();
                 //faire jouer un son de coup lorsque ennemi frappe au joueur
-                GetComponent<AudioSource>().Play();
+                GetComponent<AudioSource>().PlayOneShot(slashSon, 0.5f);
             }
         }
 
