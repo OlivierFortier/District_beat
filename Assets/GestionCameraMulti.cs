125514fd2d1d85a4b7e24377f419a9240e7ec674
Olivier Fortier <olivier_fortier@outlook.com>
2020-12-14 21:20:08 -0500

    gestion caméra multi fonctionnelle

---
+++ Assets/GestionCameraMulti.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class GestionCameraMulti : MonoBehaviour
+{
+
+   public CinemachineTargetGroup groupeCamera;
+//    private List<GameObject> lesJoueurs = new List<GameObject>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var arrJoueurs = GameObject.FindGameObjectsWithTag("joueur");
+        // Cinemachine.CinemachineTargetGroup.Target cible;
+
+        foreach (GameObject joueur in arrJoueurs) {
+            groupeCamera.AddMember(joueur.transform, 1f, 3.12f);
+        }
+    }
+
+}
